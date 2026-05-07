82dc1155afbc1de54ed2ab888104270f2e00b70a
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2026-05-07 14:23:47 +0530

    feat: implement user registration with validation and session handling

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,37 @@
+import { connectDB } from "@/lib/mongodb";
+import { User } from "@/models/User";
+import { signIn } from "next-auth/react";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const { email, password, name } = await request.json();
+    try {
+        if (!email || !password || !name) {
+            return NextResponse.json({ error: "All fields are required" }, { status: 400 })
+        }
+
+        console.log(email, password, name)
+        await connectDB();
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
+        }
+
+        await User.create({
+            email,
+            name,
+            password,
+            role: "admin"
+        })
+
+    
+        return NextResponse.json({ message: "Registerd successful" }, { status: 200 })
+
+
+    } catch (erroe) {
+        return NextResponse.json({ error: "Internal server error " }, { status: 500 })
+    }
+
+
+}
--- components/register-form.tsx
+++ components/register-form.tsx
@@ -19,16 +19,18 @@ import { signIn, useSession } from "next-auth/react"
 import Link from "next/link"
 import { useRef, useState } from "react"
 import toast from 'react-hot-toast';
-import axios from "axios";6.2
-export function RegisterForm({
+import axios from "axios"; 
+
+
+export  function RegisterForm({
     className,
     ...props
 }: React.ComponentProps<"div">) {
- 
-    const { data,status } = useSession();
-    console.log("session data data", data, "session status", status);
+
+    const { data, status } = useSession();
+    
     if (data) {
-        
+
         console.log("session data in register form", data?.user.name);
     }
 
@@ -38,82 +40,86 @@ export function RegisterForm({
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        console.log("email", email, "password", password, "name", name);
+        
         if (!email || !password || !name) {
             toast.error("Please fill in all fields");
             return;
         }
 
-        const res = axios.post("/api/auth/register", { email, password, name })
-            .then((res:any) => {
-                toast.success("Registration successful! Please log in.")
-            })
-            .catch((err:any) => {
-                console.error("Registration error:", err);
-                toast.error(err.response?.data?.message || "Registration failed");
+        const res = await axios.post("/api/auth/register", { email, password, name })
+        toast.success("Registration successful! .")
+
+        if (res.status === 200) {
+            await signIn("credentials", {
+                email, password,
+                callbackUrl: "/",
+                redirect: false,
             });
+
+
+          
+        }
     }
 
+        return (
+            <div className={cn("flex flex-col gap-6", className)} {...props}>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Register for an account</CardTitle>
+                        <CardDescription>
+                            Enter your details below to create an account
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form onSubmit={handleSubmit}>
+                            <FieldGroup>
+                                <Field>
+                                    <FieldLabel htmlFor="name">Name</FieldLabel>
+                                    <Input
+                                        id="name"
+                                        type="text"
+                                        placeholder="John Doe"
+                                        // required
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </Field>
+                                <Field>
+                                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        // required
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </Field>
+                                <Field>
+                                    <div className="flex items-center">
+                                        <FieldLabel htmlFor="password">Password</FieldLabel>
+                                    </div>
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        // required
 
-    return (
-        <div className={cn("flex flex-col gap-6", className)} {...props}>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Register for an account</CardTitle>
-                    <CardDescription>
-                        Enter your details below to create an account
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form onSubmit={handleSubmit}>
-                        <FieldGroup>
-                            <Field>
-                                <FieldLabel htmlFor="name">Name</FieldLabel>
-                                <Input
-                                    id="name"
-                                    type="text"
-                                    placeholder="John Doe"
-                                    // required
-                                    onChange={(e) => setName(e.target.value)}
-                                />
-                            </Field>
-                            <Field>
-                                <FieldLabel htmlFor="email">Email</FieldLabel>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    // required
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </Field>
-                            <Field>
-                                <div className="flex items-center">
-                                    <FieldLabel htmlFor="password">Password</FieldLabel>
-                                </div>
-                                <Input
-                                    id="password"
-                                    type="password"
-                                    // required
-
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </Field>
-                            <Field>
-                                <Button type="submit" >
-                                    Register
-                                </Button>
-                                <Button variant="outline" type="button" onClick={() => signIn("google", { callbackUrl: "/" })}>
-                                    Register with Google
-                                </Button>
-                                <FieldDescription className="text-center">
-                                    Already have an account? <Link href="/auth/login">Login</Link>
-                                </FieldDescription>
-                            </Field>
-                        </FieldGroup>
-                    </form>
-                </CardContent>
-            </Card>
-        </div>
-    )
-}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </Field>
+                                <Field>
+                                    <Button type="submit" >
+                                        Register
+                                    </Button>
+                                    <Button variant="outline" type="button" onClick={() => signIn("google", { callbackUrl: "/" })}>
+                                        Register with Google
+                                    </Button>
+                                    <FieldDescription className="text-center">
+                                        Already have an account? <Link href="/auth/login">Login</Link>
+                                    </FieldDescription>
+                                </Field>
+                            </FieldGroup>
+                        </form>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
